ea6ff4ad108f5d29b88b4f3191ad5cf1a76bd5f6
unknown <vijaychaurasiya911@gmail.com>
2024-11-11 21:42:14 +0530

    user api

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class MainController {
+
+    @GetMapping("/getUser")
+    public String getUser(@PathVariable int userId){
+        System.out.println("userId"+ userId);
+        return null;
+    }
 
 }
