9b5434065f12ea3c6b028f4482f7d9102b9c6f4c
unknown <vijaychaurasiya911@gmail.com>
2024-11-11 21:52:40 +0530

    ommiti

---
+++ src/main/java/com/citibank/hackthon/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.citibank.hackthon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class MainController {
+
+    @GetMapping("/getUser")
+    public String getUser(@PathVariable int userId){
+        System.out.println("userId"+ userId);
+        return null;
+    }
+
+}
