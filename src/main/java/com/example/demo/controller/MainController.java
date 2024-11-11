a3b9c3d37a4ba39a04454d928a1488660cdc9012
vijay <vijay.chaurasiya@beyondsquare.com>
2024-11-11 21:07:09 +0530

    Signed-off-by: vijay <vijay.chaurasiya@beyondsquare.com>

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+    public void main() {
+        System.out.println("Hello World");
+    }
+}
